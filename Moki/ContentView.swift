5fcad83076755d75f528764fa1fb50b7913956de
xuwenchao <842390367@qq.com>
2025-11-23 20:28:39 +0800

    Initial Commit

---
+++ Moki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moki
+//
+//  Created by xuwecnhao on 11/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moki/MokiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MokiApp.swift
+//  Moki
+//
+//  Created by xuwecnhao on 11/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MokiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
