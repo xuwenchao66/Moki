5b9eb2d39048f5d4d0bf38fdc2fdc5d381bd9e6c
xuwenchao <842390367@qq.com>
2025-12-20 10:12:26 +0800

    feat: style

--- Moki/MokiApp.swift
+++ Moki/MokiApp.swift
@@ -26,11 +26,24 @@ struct MokiApp: App {
 }
 
 func configureAppearance() {
+  // 统一导航栏与 body 背景色（便于维护：只改 Assets.xcassets）
   // https://stackoverflow.com/questions/58442508/swiftui-remove-navigationbar-bottom-border
+
   let appearance = UINavigationBarAppearance()
   appearance.shadowColor = .clear
-  UINavigationBar.appearance().standardAppearance = appearance
-  UINavigationBar.appearance().scrollEdgeAppearance = appearance
+
+  // 标题颜色跟随前景色
+  let titleColor = (UIColor(named: "foreground") ?? .label)
+  appearance.titleTextAttributes = [.foregroundColor: titleColor]
+  appearance.largeTitleTextAttributes = [.foregroundColor: titleColor]
+
+  let navBar = UINavigationBar.appearance()
+  navBar.standardAppearance = appearance
+  navBar.scrollEdgeAppearance = appearance
+  navBar.compactAppearance = appearance
+
+  // 导航栏按钮/返回箭头颜色
+  navBar.tintColor = titleColor
 }
 
 func configureAppDependencies() {
